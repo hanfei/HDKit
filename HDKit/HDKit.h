eb5cc822367264ab0041b39dbcd765812f978a2d
harvey <dhanf@163.com>
2014-08-08 14:56:59 +0800

    Init the project

---
+++ HDKit/HDKit.h
@@ -0,0 +1,13 @@
+//
+//  HDKit.h
+//  HDKit
+//
+//  Created by harvey.ding on 8/8/14.
+//  Copyright (c) 2014 harvey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HDKit : NSObject
+
+@end
---
+++ HDKit/HDKit.m
@@ -0,0 +1,13 @@
+//
+//  HDKit.m
+//  HDKit
+//
+//  Created by harvey.ding on 8/8/14.
+//  Copyright (c) 2014 harvey. All rights reserved.
+//
+
+#import "HDKit.h"
+
+@implementation HDKit
+
+@end
